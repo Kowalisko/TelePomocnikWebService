d2b9afa42e29716dbe9c0debd51899137c898b52
Rafał <telepomocnik@gmail.com>
2015-08-19 13:46:23 +0200

    First

---
+++ TelePomocnikWebService/Controllers/InformationController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TelePomocnikWebService.Controllers
+{
+    public class InformationController : Controller
+    {
+        //
+        // GET: /Information/
+
+        public JsonResult GetInformations()
+        {
+            TelePomocnikDbEntities db = new TelePomocnikDbEntities();
+            DateTime from = DateTime.Now.AddDays(-2);
+            var informations = db.MustCallInformation.ToList();
+            var info = "";
+
+            foreach (var item in informations)
+            {
+                info += item.DateMustContact + ": " + item.Telemarketer.Name + " " + item.Telemarketer.Surname + " do " + item.Contact.Name + " " + item.Contact.Surname + " z informacją: " + item.Information + Environment.NewLine;
+            }
+
+            return Json(info, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
