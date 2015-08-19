0519bbb6bec95334811803e2b7ebb22150a6fbaf
Rafał <telepomocnik@gmail.com>
2015-08-19 15:34:00 +0200

    jedena

--- TelePomocnikWebService/Controllers/InformationController.cs
+++ TelePomocnikWebService/Controllers/InformationController.cs
@@ -20,7 +20,7 @@ namespace TelePomocnikWebService.Controllers
 
             foreach (var item in informations)
             {
-                info += item.DateMustContact + ": " + item.Telemarketer.Name + " " + item.Telemarketer.Surname + " do " + item.Contact.Name + " " + item.Contact.Surname + " z informacją: " + item.Information + "^";
+                info += item.DateMustContact + ": " + item.Telemarketer.Name + " " + item.Telemarketer.Surname + " do " + item.Contact.Name + " " + item.Contact.Surname + " z informacją: " + item.Information + "XYZINFOXYZ";
             }
 
             return Json(info, JsonRequestBehavior.AllowGet);
